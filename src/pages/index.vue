54eb242dc2476358e6d955422dc1e59c1dc6256c
liufan1994 <liufan94@163.com>
2018-12-03 21:30:32 +0800

    开头

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+/*
+ * @Author: lf
+ * @Date: 2018-12-03 17:48:24
+ * @Last Modified by: lf
+ * @Last Modified time: 2018-12-03 21:29:47
+ * @文件说明: 首页页面
+ */
+<template>
+    <div id="app">
+        <div class="index">
+            <div class="head">我是头部</div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+<style>
+</style>
